2dbb5f096d1b18f7b506d37b2f195eb9d8c31391
Chris Skilton <chriss@brandwatch.com>
2014-10-14 20:45:17 +0100

    first step in modularising the module

--- js/index.js
+++ js/index.js
@@ -1,46 +1,68 @@
-var SCHEDULER_INTERVAL = 50;
-var LOOKAHEAD_DURATION = 0.1;
-var BEAT_DURATION = 0.25;
-var NOTE_DURATION = 0.1;
+var schedulerApp = (function(){
+    var SCHEDULER_INTERVAL = 50;
+    var LOOKAHEAD_DURATION = 0.1;
 
-var nextBeatTime = 0.0;
-var scheduler;
-var audio = new AudioContext();
+    var TEMPO = 120;
+    var BEAT_DURATION = 60.0 / TEMPO;
+    var NOTE_DURATION = 0.1;
 
-function incrementBeat(){
-    nextBeatTime += BEAT_DURATION;
-}
+    var nextBeatTime = 0.0;
+    var scheduler;
+    var audio = new AudioContext();
+    var oscillatorGen;
 
-function scheduleSound(){
-    var osc = audio.createOscillator();
-
-    osc.frequency.value = 100;
-
-    osc.connect(audio.destination);
-
-    osc.start(nextBeatTime);
-    osc.stop(nextBeatTime + NOTE_DURATION);
-}
-
-function scheduleAudioEvents(){
-    while(nextBeatTime < audio.currentTime + LOOKAHEAD_DURATION){
-        scheduleSound();
-        incrementBeat();
+    function incrementBeat(){
+        nextBeatTime += BEAT_DURATION;
     }
-}
 
-function schedule(){
-    scheduler = setTimeout(function(){
-        scheduleAudioEvents();
+    function setOscGen(func){
+        oscillatorGen = func;
+    }
+
+    oscillatorGen = function(context){
+        var osc = context.createOscillator();
+
+        return osc;
+    };
+
+    function scheduleSound(oscillator){
+        oscillator.connect(audio.destination);
+        oscillator.start(nextBeatTime);
+        oscillator.stop(nextBeatTime + NOTE_DURATION);
+    }
+
+    function setupAudioEvent(){
+        var sound = oscillatorGen(audio);
+
+        scheduleSound(sound);
+    }
+
+    function scheduleAudioEvents(){
+        while(nextBeatTime < audio.currentTime + LOOKAHEAD_DURATION){
+            setupAudioEvent();
+            incrementBeat();
+        }
+    }
+
+    function schedule(){
+        scheduler = setTimeout(function(){
+            scheduleAudioEvents();
+            schedule();
+        }, SCHEDULER_INTERVAL);
+    }
+
+    function start(){
+        clearTimeout(scheduler);
         schedule();
-    }, SCHEDULER_INTERVAL);
-}
+    }
 
-document.querySelector('#start').onclick = function(){
-    clearTimeout(scheduler);
-    schedule();
-};
+    function stop(){
+        clearTimeout(scheduler);
+    }
 
-document.querySelector('#stop').onclick = function(){
-    clearTimeout(scheduler);
-};
+    return {
+        start: start,
+        stop: stop,
+        setOscGen: setOscGen
+    };
+})();
